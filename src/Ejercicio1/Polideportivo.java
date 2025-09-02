b7112aea85d6dafae4676c0825e2aa6d0579768a
Flor2345 <flor2016.disney@gmail.com>
2025-09-02 12:39:04 -0300

    Clase Polideportivo

---
+++ src/Ejercicio1/Polideportivo.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Polideportivo implements IinstalacionDeportiva {
+
+	//Variables miembro de la clase
+	public static final int tipoInstancia = 1;
+	
+	//Metodos
+	public int getTipoDeInstalacion() {
+		return tipoInstancia;
+	}
+	
+
+}
